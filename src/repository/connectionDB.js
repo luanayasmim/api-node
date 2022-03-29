18960ed17b1f54c676a48616b5450c699ae580c0
Luana Yasmim Lourenço <luanayasmim11@outlook.com>
2022-03-29 20:41:41 -0300

    'aula29-03-2022'

---
+++ src/repository/connectionDB.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise'; // Um funcionalidade que o mysql2 tem é a possibilidade de usar promises
+
+async function connect(){
+    const connection = await mysql.createConnection({
+        host: 'localhost', 
+        user: 'root',
+        password: '',
+        database: 'clinicaDB'
+    });
+    return connection;
+};
+
+export default {connect}; //Utilizando as chaves é possivel exportar mais de um.
