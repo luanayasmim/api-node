11979fcbae80dc16025db58d6f3033cab5ee7cf1
Luana Yasmim Lourenço <luanayasmim11@outlook.com>
2022-04-10 16:22:31 -0300

    aula 07-04-2022

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,20 +1,38 @@
-import express  from "express";
-
-/*** Uma API nunca pode ter o mesmo metodo para a mesma url
- *  O navegador só enxerga metodos GET
- * 
- */
+import express, { request, response } from 'express';
+import db from '../../service/userService.js';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
-//envia informações ao frontend
-router.get('/', (request, response) => {
-  response.status(200).json({message: 'Deu tudo certo na primeira requisição'});
-});
-//adiciona infomações 
-router.post('/', async (request, response)=>{
-  const {email, password, user} = request.body;
-  awaitdb.insertUser(email, password, user);
-  response.status(201).json({message:'Cadastrei um novo usuário!'});
+
+router.get('/',(request, response) => {
+  response.status(200).json({message: 'Deu tudo certo na nossa primeira requisição'});
 });
 
+router.post('/', [
+  //Middleware
+  body("email").isEmail().withMessage('O email informado é inválido'),
+  // body(user).is().withMessage(''),
+  body("password").isEmpty().withMessage('Informe a senha!'),
+], async (request, response) => {
+  //Validação dos dados (request)
+  const errors=validationResult(request);
+
+  if(!errors.isEmpty()){
+    return response.status(400).json({message: errors.array()});
+  }
+
+  const email = request.body.email;
+  const password = request.body.password;
+  const user = request.body.user;
+
+  //Dá para fazer dessa outra forma, mais prática
+  //const {email,password,user} = request.body; 
+
+  await db.insertUser(email, password, user);
+
+  response.status(201).json({message: 'Cadastrei um novo usuário'});
+});
+ 
 export default router;
+
+//nunca poderemos ter o mesmo método para o mesmo url.
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,10 +1,12 @@
 import database from '../repository/connectionDB.js';
 
 async function insertUser(email, password, user){
-    const conn=await database.connect();
-    const sql='INSERT INTO tbl_usuarios VALUES(?, ?, ?);';
-    const insertData=[email, password, user];
-    await conn.query(sql, insertData);
-    conn.end();
+  const conn = await database.connect();
+
+  const sql = 'INSERT INTO tbl_usuario (email, senha, usuario) VALUES (?,?,?)';
+  const insertData = [email, password, user]; //parametro da função
+  await conn.query(sql, insertData); //vai ser uma promisse
+  conn.end();
 }
+
 export default {insertUser};
